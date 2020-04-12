87a084125de09073747aed3b73cdfb69b4ddaf6b
ShineySun <ksp2246@naver.com>
2020-04-12 16:41:09 +0900

    modify opencv tools

--- src/olaf/src/opencv/src/recalibration.py
+++ src/olaf/src/opencv/src/recalibration.py
@@ -5,15 +5,17 @@ import numpy as np
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
 from sensor_msgs.msg import CompressedImage
+import time
  
 class VIDEOS():
     def __init__(self):
         self.selecting_sub_image = "raw"
-        self.fps = 20.0
-        self.width= 1280
-        self.height=720
-        self.fcc = cv2.VideoWriter_fourcc('D', 'I', 'V', 'X')
-        self.out = cv2.VideoWriter('kimvav.avi', self.fcc, self.fps, (self.width, self.height))
+        self.fps = 30.0
+        self.width = 1280
+        self.height = 720
+        self.count = 1
+        # self.fcc = cv2.VideoWriter_fourcc('D', 'I', 'V', 'X')
+        # self.out = cv2.VideoWriter('/home/Desktop/test1.avi', self.fcc, self.fps, (self.width, self.height))
         self._sub = rospy.Subscriber('/usb_cam/image_raw', Image, self.callback, queue_size=1)
         self.bridge = CvBridge()
         rospy.on_shutdown(self.cleanup)
@@ -22,9 +24,12 @@ class VIDEOS():
     def callback(self, image_msg):
         cv_image = self.bridge.imgmsg_to_cv2(image_msg, "bgr8")
         dst = cv2.flip(cv_image, -1)
-        self.out.write(dst)
+        # self.out.write(dst)
+        cv2.imwrite("/home/nvidia/Desktop/video_data/frame%d.png" % self.count, dst)
+        self.count = self.count + 1
         cv2.imshow('cv_gray', dst)
         cv2.waitKey(1)
+        time.sleep(1)
 
     def cleanup(self):
         print('shutting down vision node.')
