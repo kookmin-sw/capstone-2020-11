08acb223b05300b0bd5433ad70fa0937cbfd9d31
ycw1879 <ycw1879@gmail.com>
2020-05-15 14:49:49 +0900

    version upgrade

--- src/olaf/src/lane_recognization/new_angle_lane.py
+++ src/olaf/src/lane_recognization/new_angle_lane.py
@@ -1,56 +1,41 @@
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
 import os
 
-width = 1920
-height = 1080
-lower_light = np.array([0, 0, 0])
-upper_light = np.array([240, 240, 130]) # [255, 255, 130]
+width = 1080
+height = 720
+lower_light = np.array([0, 0, 0]) # ([0, 0, 0]
+upper_light = np.array([255, 255, 130]) # [255, 255, 130]  ([240, 240, 130])
 edge_min = 20   # 20
 edge_max = 60   # 50
-roi_h1_gap = 50
-roi_h2_gap = 200
-roi_w_gap = 500
 centor_gap = 100
 hough_min_lengh = 10
 hough_max_gap = 10
-# vertices = np.array([[10,500],[10,300],[300,200],[500,200],[800,300],[800,500],], np.int32)
-vertices = np.array([[0, height], [0, height - roi_h1_gap], [width // 2 - roi_w_gap, height - roi_h2_gap]
-                        , [width // 2 + roi_w_gap, height - roi_h2_gap], [width, height - roi_h1_gap], [width, height]],dtype=np.int32)
 rank = 10
-
-# def Except_light():
-
 
 def Edge_Recalibration(img):
     global edge_min, edge_min
     result = cv2.Canny(img, edge_min, edge_max)
     return result
 
-def Region_Of_Interest(img, vertices, color3=(255, 255, 255), color1=255):  # ROI 셋팅
-    mask = np.zeros_like(img)  # mask = img와 같은 크기의 빈 이미지
-    if len(img.shape) > 2:  # Color 이미지(3채널)라면 :
-        color = color3
-    else:  # 흑백 이미지(1채널)라면 :
-        color = color1
-    # vertices에 정한 점들로 이뤄진 다각형부분(ROI 설정부분)을 color로 채움
-    cv2.fillPoly(mask, vertices, color)
-    # 이미지와 color로 채워진 ROI를 합침
-    ROI_image = cv2.bitwise_and(img, mask)
-    return ROI_image
-
 if __name__ == '__main__':
     cap = cv2.VideoCapture('test1.avi')
     while(cap.isOpened()):
+        hist = []
+        for i in range(0, 1580):
+            hist.append(0)
+        # hist = np.array([0 for _ in range(1580), dytpe=uint8])
         ret, frame = cap.read()
         image = cv2.resize(frame, dsize=(width, height), interpolation=cv2.INTER_AREA)
+        frame = image.copy()
         hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(hsv, lower_light, upper_light)
         kernel = np.ones((7, 7), np.uint8)
         result = cv2.dilate(mask, kernel, iterations=3)
         result = cv2.erode(result, kernel, iterations=1)
         res = cv2.bitwise_and(frame, frame, mask=result)
-        cv2.imshow('res', res)
+        # cv2.imshow('hsv', res)
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         edge = Edge_Recalibration(gray)
         # roi = Region_Of_Interest(gray, vertices)
@@ -60,20 +45,62 @@ if __name__ == '__main__':
                 x1, y1, x2, y2 = line.reshape(4)
                 cv2.line(edge, (x1, y1), (x2, y2), 255, 10)
         view_tp = np.zeros_like(image)
+        edge = cv2.bitwise_and(edge, edge, mask=result)
+        edge = cv2.erode(edge, kernel, iterations=1)
+        edge = cv2.dilate(edge, kernel, iterations=1)
+        new_lines = []
+        new_x = []
+        lines_left = []
+        lines_right = []
         lines = cv2.HoughLinesP(edge, 1, np.pi / 360, 100, hough_min_lengh, hough_max_gap)
         if lines is not None:
             for line in lines:
                 x1, y1, x2, y2 = line.reshape(4)
-                cv2.line(image, (x1, y1), (x2, y2), (255, 0, 0), 5)
-                cv2.line(view_tp, (x1, y1), (x2, y2), (255, 255, 255), 5)
-        rest = cv2.bitwise_and(view_tp, res)
-        cv2.imshow('frame', rest)
+                dx = x2 - x1
+                if dx == 0:
+                    continue
+                dy = y2 - y1
+                m = dy / dx
+                if abs(m) > 0.2 and abs(m) < 300:
+                    tp = m * x1
+                    b = y1 - tp
+                    start_y = height
+                    start_x = int((start_y - b) / m)
+                    if start_x <= -250 or start_x > width+250:
+                        continue
+                    hist[start_x + 250] = hist[start_x + 250] + 1
+                    end_y = 0
+                    end_x = int((end_y - b) / m)
+                    new_lines.append([start_x, start_y, end_x, end_y])
+                    new_x.append(start_x)
+                    if start_x < (width // 2):
+                    #     lines_left.append([start_x, start_y, end_x, end_y])
+                        cv2.line(image, (start_x, start_y), (end_x, end_y), (255, 0, 0), 2)
+                    else:
+                    #     lines_right.append([start_x, start_y, end_x, end_y])
+                        cv2.line(image, (start_x, start_y), (end_x, end_y), (0, 0, 255), 2)
+        max(hist)
+        # rest = cv2.bitwise_and(view_tp, res)
+        # rest = cv2.bitwise_and(view_tp, view_tp, mask=result)
+        # cv2.imshow('total', view_tp)
         cv2.imshow('origin', image)
-
-
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        cv2.waitKey(1)
+        x=np.linspace(1, 1580, 1579)
+        idx = hist.index(max(hist))
+        idx_low = 0
+        idx_up = 1580
+        if idx - 100 < 0:
+            idx_low = 0
+        else:
+            idx_low = idx - 100
+        if idx + 100 >= 1580:
+            idx_up = 1580
+        else:
+            idx_up = idx + 100
+        first = sum(hist[idx_low:idx_up])
+        # hist[idx_low:idx_up] = 0
+        plt.plot(x, hist)
+        plt.show()
     cap.release()
     cv2.destroyAllWindows()
 
