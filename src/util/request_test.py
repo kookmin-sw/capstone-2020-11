fd84f89abeb64aba1c582bc6d907529983bc46dc
ShineySun <ksp2246@naver.com>
2020-05-27 15:16:24 +0000

    Network plot data

--- src/util/request_test.py
+++ src/util/request_test.py
@@ -1,6 +1,13 @@
 import requests
 import time
 
+import matplotlib.pyplot as plt
+import numpy as np
+
+t_list = list()
+
+f = open("KMU_wifi.txt", mode = 'w')
+
 def timer(_time):
     timer_st = time.time()
     timer_et = time.time()
@@ -22,17 +29,25 @@ print(data)
 print(data['isRunning'])
 
 tot_time = 0
-for i in range(100):
+for i in range(1000):
     st = time.time()
     res = requests.get(ip2 + ':3000/running', headers=headers)
     et = time.time()
+    #t_list.append((et-st)*1000)
+    f.write(str((et-st)*1000)+'\n')
+    
+    
+    
+    
+    
     print(i, (et-st)*1000, res.json())
     tot_time += (et-st)*1000
-    
-    timer(1)
+
+    timer(0.5)
         
 
 
 print((t_et-t_st)*1000)
 print(tot_time)
-print(tot_time/100)
+print(tot_time/1000)
+#print(t_list)
