748f1f71795814853637f77f9d4c7da2757da471
ShineySun <ksp2246@naver.com>
2020-05-27 09:34:01 +0900

    mod waypoint_generator.py

--- src/olaf/src/waypoints_generator/src/waypoint_generator.py
+++ src/olaf/src/waypoints_generator/src/waypoint_generator.py
@@ -18,17 +18,7 @@ way_points = [
 {'idx' : 0, 'name' : 'START_POINT', 'x_location' : 0, 'y_location' : 0, 'yaw': 0, 'lds_detect' : 'NONE', 'line_detect' : 'OFF', "drive" : "ROTATION", "is_intersact": 0,"AJ_NODE" : "1,4"},
 {'idx' : 1, 'name' : 'TLAB', 'x_location' : 0 'y_location' : 2, 'yaw': 90, 'lds_detect' : 'RIGHT', 'line_detect' : 'ON', "drive" : "FOWARD", "is_intersact": 0,"AJ_NODE" : "2"},
 {'idx' : 2, 'name' : 'MAN_TOILET', 'x_location' : 0, 'y_location' : 4, 'yaw': 90, 'lds_detect' : 'NONE', 'line_detect' : 'OFF', "drive" : "TOTAL", "is_intersact": 1, "AJ_NODE" : "2"}
-
 ]
-way_point.append(tmp)
-way_point.append(tmp1)
-way_point.append(tmp2)
-way_point.append(tmp3)
-way_point.append(tmp4)
-way_point.append(tmp5)
-way_point.append(tmp6)
-way_point.append(tmp7)
-way_point.append(tmp8)
 
 with open('way_point.json','w') as make_file:
-    json.dump(way_point,make_file,ensure_ascii=False)
+    json.dump(way_points,make_file)
